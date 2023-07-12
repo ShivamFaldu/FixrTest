1f0d934bd2358b2baad8612815be6e8d051d196f
shivam <vashish.shivam@gmail.com>
2023-07-12 15:33:12 +0100

    Fixr-Test

--- ticket/serializers.py
+++ ticket/serializers.py
@@ -23,10 +23,10 @@ class OrderSerializer(serializers.ModelSerializer):
         #this update method was only designed to update cancellation and nothing else
         if instance.cancelled:
             raise ValidationError("This order has already been cancelled.")
-        if instance.quantity != validated_data["quantity"]:
-            raise ValidationError("The quantity cancelled does not match the order quantity")
-        if instance.ticket_type != validated_data["ticket_type"]:
-            raise ValidationError("Ticket Types do not match")
+        if validated_data["quantity"]:
+            raise ValidationError("Cannot update this field at the moment")
+        if validated_data["ticket_type"]:
+            raise ValidationError("Cannot update this field at the moment")
         if validated_data["cancelled"]:
             instance.cancel_order
         return instance
